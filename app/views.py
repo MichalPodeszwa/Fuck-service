acde616cc4ef132112f6c3f2e7c5333ac9f27524
Michał Podeszwa <michal.podeszwa@gmail.com>
2014-10-04 00:19:18 +0200

    Initial working version with no db

--- app/views.py
+++ app/views.py
@@ -1,82 +1,47 @@
-from flask import Flask
-from flask import render_template, request, jsonify, flash, redirect
-from app import app, mongo, single_forms_file, forms_file
+from flask import render_template
+from app import app, single_forms_file, forms_file
 from werkzeug.exceptions import NotFound
-import mimerender
-import json
-from bson.objectid import ObjectId
-
-mimerender = mimerender.FlaskMimeRender()
-
-render_xml = lambda content: '<content>{}</content>'.format(content)
-render_json = lambda **args: jsonify(args)
-render_html = lambda content: render_template('base.html', content=content)
-render_txt = lambda content: content
-
-
-@app.errorhandler(404)
-def error(*args, **kwargs):
-    flash("Use proper URLs")
-    return redirect('/')
 
 
 @app.route('/')
-def index(*args, **kwargs):
-    return render_template("index.html")
+def index():
+    return render_template(
+        "index.html",
+        singles=[form for form in single_forms_file],
+        doubles=[form for form in forms_file]
+    )
 
 
-@app.route('/stats')
-def stats():
-    froms = mongo.db.stats.froms.find({})
-    tos = mongo.db.stats.tos.find({})
-    return render_template("stats.html",
-                           froms=froms,
-                           tos=tos)
-
-
-def increment(username, user_from):
-    if user_from:
-        stat = mongo.db.stats.froms.find_one({"name": username})
+@app.route("/<form_name>/<person_to>/<person_from>")
+@app.route("/<form_name>/<person_from>")
+def give_a_fuck(form_name, person_from, person_to=None):
+    if form_name in single_forms_file:
+        content = single_forms_file[form_name].format(person_from=person_from)
+    elif form_name in forms_file:
+        content = forms_file[form_name].format(person_from=person_from, person_to=person_to)
     else:
-        stat = mongo.db.stats.tos.find_one({"name": username})
-    if stat:
-        stat["amount"] += 1
-        if user_from:
-            mongo.db.stats.froms.update({"_id": ObjectId(stat["_id"])}, stat)
-        else:
-            mongo.db.stats.tos.update({"_id": ObjectId(stat["_id"])}, stat)
-    else:
-        if user_from:
-            mongo.db.stats.froms.insert({"name": username, "amount": 1})
-        else:
-            mongo.db.stats.tos.insert({"name": username, "amount": 1})
+        raise NotFound()
+    return render_template(
+        "output.html",
+        content=content
+    )
 
 
-@app.route('/<form_name>/<per_to>/<per_from>')
-@app.route('/<form_name>/<per_from>')
-@mimerender(
-    default="txt",
-    html=render_html,
-    xml=render_xml,
-    json=render_json,
-    txt=render_txt
-)
-def fuck_off(form_name, per_from, per_to=None):
-    increment(per_from, True)
-    if per_to:
-        increment(per_to, False)
+# @app.route('/<form_name>/<per_to>/<per_from>')
+# @app.route('/<form_name>/<per_from>')
+# def fuck_off(form_name, per_from, per_to=None):
 
-    if form_name not in forms_file:
-        if form_name not in single_forms_file:
-            return "NOPE"
-            raise NotFound()
-    if per_to:
-        return {"content": forms_file[form_name].format(
-            per_to=per_to,
-            per_from=per_from)
-        }
-    else:
-        return {"content": single_forms_file[form_name].format(
-                per_from=per_from
-            )
-        }
+#     if form_name not in forms_file:
+#         if form_name not in single_forms_file:
+#             return "NOPE"
+#             raise NotFound()
+#     if per_to:
+#         return {"content": forms_file[form_name].format(
+#             per_to=per_to,
+#             per_from=per_from)
+#         }
+#     else:
+#         return {"content": single_forms_file[form_name].format(
+#                 per_from=per_from
+#                 )
+#         }
